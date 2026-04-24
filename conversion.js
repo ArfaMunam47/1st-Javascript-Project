3f1cab1be312ad6cd4823af479ae483b367a3f0b
ArfaMunam <arfamunam01@gmail.com>
2026-04-24 12:34:32 -0700

    Conversion Done

---
+++ conversion.js
@@ -0,0 +1,22 @@
+// conversion learning in JS
+//Number
+// let score = "345abc"
+// // console.log(score);
+// console.log(typeof score);
+// let valueInNumber= Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+//Number to String
+let string = undefined
+// console.log(scor);
+console.log(typeof string);
+let valueInString= String(string)
+console.log(valueInString);
+console.log(typeof valueInString);
+//boolean conversion
+let isLoggedIn= 0
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+//1 => true     //null => false
+//0 => false    //"arfa" => true
