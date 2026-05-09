d9a98fa21d3b20ed331eadeb871aebcb9d1826f4
Arfa Munam Butt <arfamunam01@gmail.com>
2026-05-09 02:49:44 -0700

    IIFE Concept

---
+++ IIFE.js
@@ -0,0 +1,26 @@
+// -------------- IIFE -----------------------
+// ------------ Immediately Invoked Function Expression ----------------------
+(function name(){
+    console.log(`My name is Arfa Munam`);
+    
+}) (); //semi colon is important
+//if we don't use semi colon here we'll get an error
+// name()
+(function one(){
+    console.log("Just checking IIFE");
+    
+}) ();
+
+// ------------ ARROW FUNCTION CHECKING --------------------
+( () =>{
+    console.log(`Arrow Function Testing IIFE`);
+    
+})();
+// ---------- GIVING PARAMETERS TOO --------------------
+console.log(( (num1,num2)=>num1+ num2)(33,44));
+console.log(((num1)=>num1+1)(5));
+
+//------------- Declaring Variable in ARROW FUNCTION & checking IIFE -----------------------------
+let two=( (num3,num4)=>num3*num4)(3,4)
+console.log(two);
+
