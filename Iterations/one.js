13d00cafc919643fc4fe25b06f963831388a834c
Arfa Munam Butt <arfamunam01@gmail.com>
2026-05-20 06:30:37 -0700

    20-05-2026

--- Iterations/one.js
+++ Iterations/one.js
@@ -16,13 +16,41 @@ for (let i = 0; i <= 5; i++) {
 }
 
 // Checking multiplication here
-for (let i = 0; i <= 5; i++) {
-    // console.log(`Outer loop : ${i}`);
-    for (let j = 0; j < 7; j++) {
+for (let i = 10; i <= 10; i++) {
+    // console.log(i);
+    
+    console.log(`Outer loop : ${i}`);
+    for (let j = 0; j <= 10; j++) {
     //    console.log(`Outer loop : ${i} and Inner loop : ${j}`);  
-  console.log(i + '*' + j + '=' + i * j);
-  
+console.log(i + '*' + j + '=' + i * j);
+   } 
+}
+//  PRINTING ALL TABLES
+for (let i = 1; i <=10 ; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(i +'*'+ j + '=' + i * j);    
+        
+    } 
+}
+//  ARRAY CONCEPT IN LOOP
+let myArr=["arfa", "nisha", "tayba","ayesha"]
+console.log(myArr.length);
+
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element);
+    
+    
+}
+//Break and Continue CONCEPT
+for (let i = 1; i <= 20; i++) {
+    if(i == 5){
+        console.log(`Detected 5`);
+        // break //will stop printing next numbers
+        continue //will print next numbers but in place of 5, it will print the statement that we give 
         
     }
-  
+   console.log(`Value of i is ${i}`);
+   
+    
 }
