178470a9e3f7f8f916ff15cb0ffbe0ab3f5825f9
Arfa Munam Butt <arfamunam01@gmail.com>
2026-05-20 07:00:41 -0700

    While LOOP

---
+++ Iterations/two.js
@@ -0,0 +1,26 @@
+// 20-05-2026 WORK
+// while (condition) {
+    
+// }        //SYNTAX
+// while ( 2 == 2) {
+//     console.log(`This is true`);
+    
+// }
+let i= 0
+while (i<=20) {
+    console.log(`Value is ${i}`);
+    // i++
+    i= i + 2
+    
+}
+// ARRAY CONCEPT
+let myArr= ['arfa', 'ayesha','tayba','sania']
+console.log(myArr.length);
+
+let arr= 0
+while (arr< myArr.length) {
+    console.log(`Value is ${myArr[arr]}`);
+    arr= arr + 1
+    
+    
+}
