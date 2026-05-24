f15ccadc1d3b747dadf8c9afcbd2861a6b3a22ec
Arfa Munam Butt <arfamunam01@gmail.com>
2026-05-24 04:20:47 -0700

    UPDATED

--- Iterations/six.js
+++ Iterations/six.js
@@ -22,5 +22,71 @@ if(item>=5)
     newArr.push(item)
 })
 console.log(newArr);
+//  BOOK EXAMPLE
+let book=[
+    {
+        Title:"Book-One",
+        Publish: 1980,
+        Genre:'Fiction',
+    },
+    {
+        Title:"Book-Two",
+        Publish: 1990,
+        Genre:'Non-Fiction',
+    },
+    {
+        Title:"Book-Three",
+        Publish: 1480,
+        Genre:'Fiction',
+    },
+    {
+        Title:"Book-Four",
+        Publish: 1986,
+        Genre:'History',
+    },
+    {
+        Title:"Book-Five",
+        Publish: 1880,
+        Genre:'Statistics',
+    },
+    {
+        Title:"Book-Six",
+        Publish: 1989,
+        Genre:'Fiction',
+    },
+    {
+        Title:"Book-Seven",
+        Publish: 1960,
+        Genre:'Fiction',
+    },
+    {
+        Title:"Book-Eight",
+        Publish: 2001,
+        Genre:'Non-Fiction',
+    },
+    {
+        Title:"Book-Nine",
+        Publish: 2000,
+        Genre:'Fiction',
+    },
+    {
+        Title:"Book-Ten",
+        Publish: 1999,
+        Genre:'Mathematics',
+    },
+]
+book.forEach((bk) =>{
+    console.log(bk);
+    
+})
+let num4= book.filter((bk) => bk.Publish === 1980)
+console.log(num4);
+num4= book.filter((bk) => bk.Genre === 'Non-Fiction')
+console.log(num4);
+num4= book.filter((bk) => bk.Publish >=1990 && bk.Genre=== 'Fiction')
+console.log(num4);
+
+
+
 
 
